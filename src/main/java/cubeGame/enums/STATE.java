d52bfcbb6fcb9510f3aa20e86ca11678b1fa0cc2
TCRichards <thomas.c.richards@outlook.com>
2018-08-05 12:35:14 -0400

    Delete STATE.java

--- src/main/java/cubeGame/enums/STATE.java
+++
@@ -1,9 +0,0 @@
-package main.java.cubeGame.enums;
-
-/*
- * 3 stages to the game: roll the dice, moving the die faces, and once all dice are placed, allow recording
- */
-
-public enum STATE {
- ROLL, MOVE, PLACED;
-}
